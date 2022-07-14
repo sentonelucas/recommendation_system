f93bb2c4afca1927cff8c1af6fb7ad55a5fdf2a1
sentonelucas <lucas.sentone1@gmail.com>
2022-07-14 20:50:13 -0300

    givin up on take the cents of the price because de html not permit in a easy way

--- scrap_graphic_card.py
+++ scrap_graphic_card.py
@@ -1,12 +1,12 @@
 from attr import attr
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as bs
 import pandas as pd 
 
 response = requests.get('https://lista.mercadolivre.com.br/placa-de-video')
 print(response)
 
-site = BeautifulSoup(response.text, 'html.parser')
+site = bs(response.text, 'html.parser')
 
 products = site.findAll('div', attrs={'class': 'andes-card andes-card--flat andes-card--default ui-search-result ui-search-result--core andes-card--padding-default andes-card--animated'})
 
@@ -16,13 +16,7 @@ for product in products:
     #print(title.text)
 
     link = product.find('a', attrs={'class': 'ui-search-link'})
-    #print(link['href'])
+    print(link['href'])
 
-    price_fraction = product.find('span', attrs={'class': 'price-tag-fraction'})
+    price_fraction = product.find('span', attrs={'class': 'price-tag-fraction'})    
     
-    price_cents = product.find('span', attrs={'class': 'price-tag-cents'})
-
-    if price_cents: 
-        print(price_fraction.text + ',' + price_cents.text)
-    else:
-        print(price_fraction.text + ',' + ',00')
